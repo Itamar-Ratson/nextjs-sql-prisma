c7f06fe0d8b425e27a0299f5b0a5914a75c4340a
Itamar Ratson <itamar.ratson@gmail.com>
2024-07-28 19:06:13 +0300

    added auth check

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,9 +1,20 @@
 'use server';
 
 import prisma from '@/lib/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+// import { redirect } from 'next/dist/server/api-utils';
 
 export async function createPost(formData: FormData) {
+	// auth check
+	const { isAuthenticated } = getKindeServerSession();
+	if (!(await isAuthenticated())) {
+		redirect('/api/auth/login');
+	}
+
+	// validation check here (e.g. zod)
+
 	const title = formData.get('title') as string;
 	const body = formData.get('body') as string;
 
